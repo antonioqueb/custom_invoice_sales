5d2539b818f5ade929fff3a00db644979af368d5
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-19 22:35:23 -0600

    Up

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -6,17 +7,18 @@ class SaleOrder(models.Model):
     invoice_ids = fields.Many2many('account.move', string="Facturas Relacionadas", readonly=True)
 
     def action_create_invoice(self):
-        """Generar factura agrupada para múltiples órdenes del mismo cliente."""
-        orders = self.search([
-            ('partner_id', '=', self.partner_id.id), 
-            ('invoice_status', '!=', 'invoiced')
-        ])
+        """Generar factura agrupada desde las órdenes seleccionadas."""
+        orders = self.filtered(lambda o: o.invoice_status != 'invoiced')
 
         if not orders:
-            return
+            raise UserError("Todas las órdenes seleccionadas ya están facturadas.")
+
+        partners = orders.mapped('partner_id')
+        if len(partners) > 1:
+            raise UserError("Selecciona solo órdenes del mismo cliente para facturar juntas.")
 
         invoice_vals = {
-            'partner_id': self.partner_id.id,
+            'partner_id': partners.id,
             'move_type': 'out_invoice',
             'invoice_line_ids': [],
         }
@@ -25,8 +27,10 @@ class SaleOrder(models.Model):
             for line in order.order_line:
                 invoice_vals['invoice_line_ids'].append((0, 0, {
                     'product_id': line.product_id.id,
+                    'name': line.name,
                     'quantity': line.product_uom_qty,
                     'price_unit': line.price_unit,
+                    'tax_ids': [(6, 0, line.tax_id.ids)],
                     'sale_line_ids': [(6, 0, [line.id])]
                 }))
 
@@ -37,4 +41,11 @@ class SaleOrder(models.Model):
             'invoice_status': 'invoiced'
         })
 
-        return invoice
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Factura Creada',
+            'view_mode': 'form',
+            'res_model': 'account.move',
+            'res_id': invoice.id,
+            'target': 'current'
+        }
